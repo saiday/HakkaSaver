703686544ace59ef473b6a33e662816dee17597a
Saiday <feocms@gmail.com>
2019-01-03 02:05:31 +0800

    Initial Commit

---
+++ HakkaSaver/HakkaSaverView.h
@@ -0,0 +1,13 @@
+//
+//  HakkaSaverView.h
+//  HakkaSaver
+//
+//  Created by saiday on 2019/1/3.
+//  Copyright © 2019 saiday. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface HakkaSaverView : ScreenSaverView
+
+@end
