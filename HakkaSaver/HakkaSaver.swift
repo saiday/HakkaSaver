d042e3d349384781656ef3c8fb3df42f78ed4d69
Saiday <feocms@gmail.com>
2019-02-05 00:48:08 +0800

    transparent screen saver

--- HakkaSaver/HakkaSaver.swift
+++ HakkaSaver/HakkaSaver.swift
@@ -7,25 +7,89 @@
 //
 
 import ScreenSaver
+import os
 
 @objc(HakkaSaver)
 class HakkaSaver: ScreenSaverView {
-    override func draw(_ rect: NSRect) {
-        let img = CGWindowListCreateImage(rect
-            , .optionOnScreenBelowWindow
-            , CGWindowID(self.window!.windowNumber)
-            , .bestResolution)!
+    var previewShown = false
+    var imageView: NSImageView!
+    var label: NSTextField!
+    
+    // MARK: new properties
+    typealias WindowInfo = [String: Any]
+    let ScreenSaverWindowLayer = CGWindowLevelForKey(.screenSaverWindow)
+
+    var displayID: CGDirectDisplayID? {
+        guard let devInfo = window?.screen?.deviceDescription else {
+            os_log("Could not get device information for ScreenSaverView's screen",
+                   type: .error)
+            return nil
+        }
         
-        let context = NSGraphicsContext.current?.cgContext
-        context?.draw(img, in: rect)
+        return devInfo [NSDeviceDescriptionKey("NSScreenNumber")] as? CGDirectDisplayID
     }
     
-    // TODO: extension override
-    func isOpaque() -> Bool {
-        return true
+    /// Bounds of the display the current instance of ScreenSaverView is
+    /// presented on in global screen space coordinates. `nil` if the device
+    /// description of the screen can't be accessed which _shouldn't_ happen.
+    var displayBounds: CGRect? {
+        guard let displayID = displayID else {
+            return nil
+        }
+        return CGDisplayBounds(displayID)
+    }
+
+    override init?(frame: NSRect, isPreview: Bool) {
+        super.init(frame: frame, isPreview: isPreview)
     }
     
-    func performGammaFade() -> Bool {
-        return false
+    required init?(coder decoder: NSCoder) {
+        fatalError("not impletemted")
+    }
+    
+    func screenshotImage() -> CGImage? {
+        guard let onscreenWindows = CGWindowListCopyWindowInfo(.optionOnScreenOnly, kCGNullWindowID) as! [WindowInfo]? else {
+            os_log("No windows are on screen", type: .info)
+            return nil
+        }
+        
+        guard let displayBounds = displayBounds else {
+            return nil
+        }
+        
+        // TODO: though CGWindowLevel > SCreenSaverWindowLayer but it still have chance see lock screen captured
+        if let topWindow = onscreenWindows.filter({ $0["kCGWindowLayer"] as! CGWindowLevel > ScreenSaverWindowLayer }).reversed().first {
+            os_log("aaaaaaaa had top window", type: .info)
+            NSLog("aaaaaaaa had top window")
+            return CGWindowListCreateImage(displayBounds, .optionOnScreenBelowWindow, topWindow["kCGWindowNumber"]! as! CGWindowID, .bestResolution)
+        } else {
+            os_log("aaaaaaaa no top window", type: .info)
+            NSLog("aaaaaaaa no top window")
+            return CGWindowListCreateImage(displayBounds, .optionOnScreenOnly, kCGNullWindowID, .bestResolution)
+        }
+    }
+    
+    override func draw(_ rect: NSRect) {
+        super.draw(rect)
+        
+        guard let screenShot = screenshotImage() else {
+            os_log("Could not take a screenshot")
+
+            NSColor.clear.setFill()
+            __NSRectFill(self.bounds)
+            NSColor.black.set()
+            let errorMessage:NSString = "Could not take a screenshot"
+            errorMessage.draw(at: NSPoint(x: 100.0, y: 200.0), withAttributes:nil)
+
+            return
+        }
+        
+//        let blurredScreenshot = CIImage(cgImage: screenShot).applyingClampedGaussianBlur(withSigma: Preferences.shared.blurRadius)
+        let blurredScreenshot = CIImage(cgImage: screenShot)
+
+        blurredScreenshot.draw(in: rect,
+                               from: blurredScreenshot.extent,
+                               operation: .copy,
+                               fraction: 1.0)
     }
 }
